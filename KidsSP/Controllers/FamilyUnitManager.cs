584f11ff98417403c35997ecd68968b539523c05
Cristian <i449915@student.fontys.nl>
2021-06-13 11:01:02 +0300

    Added blueprint for FamilyUnit management

---
+++ KidsSP/Controllers/FamilyUnitManager.cs
@@ -0,0 +1,34 @@
+﻿using KidsSP.Models;
+using KidsSP.Models.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KidsSP.Controllers
+{
+    public class FamilyUnitManager
+    {
+        public void AddParent(Parent parent)
+        {
+            //DBController.RegisterParent(parent);
+        }
+        public void AddChild(Child child)
+        {
+            //DBController.RegisterChild(child);
+        }
+        public void UpdateParent(Parent parent, Parent updatedParent)
+        {
+            //DBController.UpdateUser(parent, updatedParent);
+        }
+        public void UpdateChild(Child child, Child updatedChild)
+        {
+            //DBController.UpdateChild(child, updatedChild);
+        }
+        public void UpdateFamilyUnit(FamilyUnit unit, FamilyUnit updatedFamilyUnit)
+        {
+
+        }
+    }
+}
---
+++ KidsSP/Forms/ParentDashboard.cs
@@ -0,0 +1,23 @@
+﻿using KidsSP.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KidsSP
+{
+    public partial class ParentDashboard : Form
+    {
+        private FamilyUnit familyUnit;
+        public ParentDashboard(FamilyUnit familyUnit)
+        {
+            this.familyUnit = familyUnit;
+            InitializeComponent();
+        }
+    }
+}
